6351ec5b45b208868fe89d1c9463be0208e96461
Severin Ibarluzea <seveibar@gmail.com>
2025-12-20 10:04:23 -0800

    Revert "Fix assignable obstacle layer traversal (#426)" (#429)
    This reverts commit 12506f39375e1d59a13890e3e00009cb7b8bca24.

--- examples/bug-reports/bugreport26-66b0b2/bugreport26-66b0b2.fixture.tsx
+++
@@ -1,6 +0,0 @@
-// @ts-nocheck
-import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebugger"
-import bugReportJson from "./bugreport26-66b0b2.json"
-export default () => {
-  return <AutoroutingPipelineDebugger srj={bugReportJson.simple_route_json} />
-}
